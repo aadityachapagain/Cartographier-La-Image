199f2d5d274363b3c2062899d194e1784e096a26
Aaditya chapagain <chapagain.aaditya@gmail.com>
2019-01-27 00:49:41 +0545

    fixing upload and canvas issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null,
+      dimensions: null,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
+
+  onImgLoad = ({target:img}) => {
+    this.setState({dimensions:{height:img.naturalHeight,
+      width:img.naturalWidth}});
+  }
+
+  graph = () => {
+    if (!this.state.dimensions){
+      return null
+    }
+    return(
+      <div>
+        <canvas ref="canvas" width={this.state.dimensions.width} height={this.state.dimensions.height} />
+      </div>
+    )
+  }
   render() {
+    const style= {
+      height:200,
+      width:180,
+      display:'none'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input type="file" onChange={this.handleChange}/>
+        <img src={this.state.file} ref="image" onLoad={this.onImgLoad} style={style}/>
+        <p>{this.state.file?this.state.file.name:''}</p>
+        {this.graph()}
       </div>
     );
   }
